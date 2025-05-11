f978b2a88f8d8879b5317116b5cccd950469d156
silwalanish <silwalanish@gmail.com>
2025-05-11 19:57:29 +0545

    Fix basePath

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [
     TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
     react(),
